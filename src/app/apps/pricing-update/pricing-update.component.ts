9276373f053850f12aaaffb0fe620a366dbb4888
Leandro Santos Oliveira <leandro.oliveira@closer.pt>
2023-05-17 17:50:07 +0100

    start to add layout import page pricing

--- src/app/apps/pricing-update/pricing-update.component.ts
+++ src/app/apps/pricing-update/pricing-update.component.ts
@@ -1,20 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FormControl, Validators } from '@angular/forms';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-pricing-update',
   templateUrl: './pricing-update.component.html',
   styleUrls: ['./pricing-update.component.scss'],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'pt-pt'},
+  {
+    provide: DateAdapter,
+    useClass: MomentDateAdapter,
+    deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+  },
+  {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},]
 })
 export class PricingUpdateComponent {
+  
   options: FormGroup;
+  firstFormGroup: FormGroup = Object.create(null);
+  secondFormGroup: FormGroup = Object.create(null);
+  
 
-  constructor(fb: FormBuilder) {
-    this.options = fb.group({
+  constructor(formBuilder: FormBuilder,
+    @Inject(MAT_DATE_LOCALE) private _locale: string,
+
+    private _adapter: DateAdapter<any>,) {
+    this.options = formBuilder.group({
       hideRequired: false,
+      isChecked: false,
       floatLabel: 'auto',
     });
+    this.firstFormGroup = formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+    this._locale = 'pt-br';
+    this._adapter.setLocale(this._locale);
+  }
+
+  get isChecked(){
+    return this.options.controls['isChecked'].value;
   }
   // For form validator
   email = new FormControl('', [Validators.required, Validators.email]);
